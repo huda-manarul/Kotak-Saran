69d3af7e4c5dee30ca573fd264821e0e4c4d30be
huda-manarul <huda.mana.01@gmail.com>
2018-05-27 14:31:54 +0700

    Finishing

--- app/src/main/java/mana/huda/masukkan/activity/MainActivity.java
+++ app/src/main/java/mana/huda/masukkan/activity/MainActivity.java
@@ -1,59 +1,62 @@
-package mana.huda.masukkan.activity;
+package mana.huda.masukkan;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import mana.huda.masukkan.R;
-import mana.huda.masukkan.ScrollingActivity;
+import mana.huda.masukkan.activity.LoginActivity;
+import mana.huda.masukkan.activity.MatkulActivity;
 import mana.huda.masukkan.util.SharedPrefManager;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
 public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.tvResultNama)
-    TextView tvResultNama;
-    @BindView(R.id.btnLogout)
-    Button btnLogout;
-    @BindView(R.id.btnLihatMatkul)
-    Button btnLihatMatkul;
-
     SharedPrefManager sharedPrefManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ButterKnife.bind(this);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         sharedPrefManager = new SharedPrefManager(this);
 
-        tvResultNama.setText(sharedPrefManager.getSPNama());
-
-        btnLogout.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                sharedPrefManager.saveSPBoolean(SharedPrefManager.SP_SUDAH_LOGIN, false);
-                startActivity(new Intent(MainActivity.this, LoginActivity.class)
-                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
-                finish();
-            }
-        });
-
-        btnLihatMatkul.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
                 startActivity(new Intent(MainActivity.this, MatkulActivity.class));
             }
         });
     }
 
-    public void doscrolling(View view) {
-        startActivity(new Intent(MainActivity.this, ScrollingActivity.class));
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.newMessage){
+            Toast.makeText(MainActivity.this, "New Message Selected", 1000).show();
+        } else if (item.getItemId() == R.id.setting) {
+            Toast.makeText(MainActivity.this, "Setting Selected", 1000).show();
+        } else if (item.getItemId() == R.id.help) {
+            sharedPrefManager.saveSPBoolean(SharedPrefManager.SP_SUDAH_LOGIN, false);
+            startActivity(new Intent(MainActivity.this, LoginActivity.class)
+                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+            finish();
+        }
+
+        return true;
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_option_menu, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
